4a75a19f5809110c2e10a45483ecf758cec792b1
Louis Cloete <louisc363@gmail.com>
2022-07-12 22:35:44 +0200

    Removed unused imports

--- app/models.py
+++ app/models.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-
-from sqlalchemy.orm import backref
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
