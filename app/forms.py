5a9832f2e6e3c8eeecf6c3a724de958491361a97
Louis Cloete <louisc363@gmail.com>
2022-12-27 19:56:32 +0200

    Made code identical to reference impl.

--- app/forms.py
+++ app/forms.py
@@ -65,6 +65,5 @@ class EmptyForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired()])
     submit = SubmitField('Submit')
