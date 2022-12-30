1a2c7e2914445b72ed2ec11449d63da18ebd7f68
Louis Cloete <louisc363@gmail.com>
2022-12-30 17:20:51 +0200

    Translate route

--- app/translate.py
+++ app/translate.py
@@ -6,7 +6,7 @@ from app import app
 
 def translate(text, source_language, dest_language):
     if 'MS_TRANSLATOR_KEY' not in app.config or \
-            not app.config('MS_TRANSLATOR_KEY'):
+            not app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
     auth = {
         'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
