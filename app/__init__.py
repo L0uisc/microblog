c1e51ee8e8e70b6aa45d17c0f6b499216e54759c
Louis Cloete <louisc363@gmail.com>
2023-01-02 19:54:18 +0200

    auth and main blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
@@ -26,6 +26,12 @@ babel = Babel(app)
 
 from app.errors import bp as error_bp
 app.register_blueprint(error_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -61,4 +67,4 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-from app import routes, models
+from app import models
